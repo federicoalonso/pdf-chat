7803c7c701f14bb1925dc96723f3ac85cbe6e4d8
Federico Alonso <f.nicolas.alonso@gmail.com>
2023-11-01 19:56:06 -0300

    Add PDF text extraction functionality to PDF Chat App

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
 
+def get_pdf_text(pdf_files):
+    text = ""
+    for pdf_file in pdf_files:
+        pdf_reader = PdfReader(pdf_file)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 def main():
+    load_dotenv()
     st.set_page_config(page_title="PDF Chat App", page_icon=":books:", layout="wide")
 
     st.header("PDF Chat App :books:")
@@ -10,8 +20,11 @@ def main():
 
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDF file here and click on 'Process'", type=["pdf"])
-        st.button("Process")
+        pdf_doct = st.file_uploader("Upload your PDF file here and click on 'Process'", type=["pdf"], accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_doct)
+                st.success("Done!")
 
 if __name__ == "__main__":
     main()
